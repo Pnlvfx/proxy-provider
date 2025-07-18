d1831ae82aa6693a915695070feea7aa41fc2d9d
simone <simonegauli@gmail.com>
2025-07-18 03:33:19 +0200

    prepare for release

--- tools/unused.ts
+++ tools/unused.ts
@@ -1,7 +1,7 @@
 import { findUnusedExports } from '@goatjs/ts-unused-exports';
 
 const unused = await findUnusedExports({
-  ignoreFiles: ['provider.ts', 'eslint.config.js', 'jest.config.ts'],
+  ignoreFiles: ['provider.ts', 'eslint.config.js'],
   ignoreVars: ['createProxyAgent', 'AgentProvider'],
 });
 
