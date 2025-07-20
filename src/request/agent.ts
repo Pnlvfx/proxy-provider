1dd7e942be15828d3ffecff406a050983d6577fd
simone <simonegauli@gmail.com>
2025-07-20 13:52:12 +0200

    align agent with changes

--- src/request/agent.ts
+++ src/request/agent.ts
@@ -2,20 +2,25 @@ import { ProxyAgent } from 'undici';
 import { proxyProvider, type ProviderOptions } from '../provider.js';
 
 // experimental helper to help switching undici agents
-export const createProxyAgent = async (options: ProviderOptions = {}) => {
-  const provider = await proxyProvider(options);
+export const createProxyAgent = async (options: Omit<ProviderOptions, 'protocols'> = {}) => {
+  const provider = await proxyProvider({ protocols: 'http', ...options });
   let agent: ProxyAgent | undefined;
 
   return {
+    reset: provider.reset,
     getCurrentAgent: async () => {
       if (agent) return agent;
       const proxy = await provider.getCurrentProxy();
-      agent = new ProxyAgent(proxy.ip);
+      const url = proxy.urls.https ?? proxy.urls.http;
+      if (!url) throw new Error('Invalid proxy received');
+      agent = new ProxyAgent(url.url);
       return agent;
     },
     getNextAgent: async () => {
       const proxy = await provider.getNextProxy();
-      agent = new ProxyAgent(proxy.ip);
+      const url = proxy.urls.https ?? proxy.urls.http;
+      if (!url) throw new Error('Invalid proxy received');
+      agent = new ProxyAgent(url.url);
     },
   };
 };
